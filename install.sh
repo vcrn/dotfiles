de935fa43cfdb6499cf977c3ec178cb7b3a198ba
Victor Nilsson <79985743+vcrn@users.noreply.github.com>
2023-05-02 21:29:39 +0200

    Add dependencies

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "Updating system..."
 pacman -Syu
 echo "Installing dependencies..."
-pacman -S feh arandr autorandr alacritty neovim ttf-hack pcmanfm firefox zsh i3-scrot unzip npm lxappearance
+pacman -S feh arandr autorandr alacritty neovim ttf-firacode-nerd ttf-hack pcmanfm firefox zsh i3-scrot unzip npm lxappearance oto-fonts-emoji
 
 echo "Setting zsh as shell"
 chsh -s /usr/bin/zsh
