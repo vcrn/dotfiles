337ca3f22defdf41d0c929c6ff49a34031375e8f
Victor Nilsson <79985743+vcrn@users.noreply.github.com>
2023-10-18 09:45:30 +0200

    Update telescope tag in plugins.lua
    Old version and newest (0.1.4) causes breakage

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -23,7 +23,7 @@ return require("packer").startup(function(use)
   use("nvim-treesitter/nvim-treesitter")
   use({
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.0",
+    tag = "0.1.3",
     requires = { { "nvim-lua/plenary.nvim" } },
   })
   use({
